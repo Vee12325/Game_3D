0f8948a63413f979ea57549f4c8b5f8c8c70d445
Vee12325 <144508918+Vee12325@users.noreply.github.com>
2025-09-15 15:03:10 +0700

    Add mob squashing, player death, and animation logic

--- 3d-squash-the-creeps/Mob.gd
+++ 3d-squash-the-creeps/Mob.gd
@@ -1,9 +1,20 @@
 extends CharacterBody3D
 
 # Minimum speed of the mob in meters per second.
-@export var min_speed = 10
+@export var min_speed = 8
 # Maximum speed of the mob in meters per second.
-@export var max_speed = 18
+@export var max_speed = 15
+signal squashed
+
+var anim_player: AnimationPlayer
+
+func _ready():
+	# หา AnimationPlayer ที่อยู่ใน scene tree ของ mob
+	anim_player = find_child("AnimationPlayer", true, false)
+	if anim_player:
+		anim_player.play("CharacterArmature|Flying_Idle")
+	else:
+		push_error("❌ AnimationPlayer not found in mob scene!")
 
 func _physics_process(_delta):
 	move_and_slide()
@@ -16,8 +27,7 @@ func initialize(start_position, player_position):
 	# Rotate this mob randomly within range of -45 and +45 degrees,
 	# so that it doesn't move directly towards the player.
 	rotate_y(randf_range(-PI / 4, PI / 4))
-
-	# We calculate a random speed (integer)
+	
 	var random_speed = randi_range(min_speed, max_speed)
 	# We calculate a forward velocity that represents the speed.
 	velocity = Vector3.FORWARD * random_speed
@@ -25,5 +35,12 @@ func initialize(start_position, player_position):
 	# in order to move in the direction the mob is looking.
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
-func _on_visible_on_screen_notifier_3d_screen_exited():
+func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
+	queue_free()
+	
+func squash():
+	if anim_player:
+		anim_player.play("CharacterArmature|Death")
+		await anim_player.animation_finished
+	squashed.emit()
 	queue_free()
